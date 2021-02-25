6e7691b2870f23246e85f032531710d5a10e62bf
mw18 <67554339+mw18@users.noreply.github.com>
2021-02-25 16:10:48 -0600

    deploying

--- server.js
+++ server.js
@@ -13,7 +13,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workoutdb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
